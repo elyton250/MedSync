ffd8886c5ce26f154e38090172e0e18b3565dd8a
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-07-11 16:47:54 +0200

    check if patient already in the DB

--- app/api_routes/post.py
+++ app/api_routes/post.py
@@ -6,6 +6,8 @@ post_routes = Blueprint('post_routes', __name__)
 @post_routes.route('/add_patient', methods=['POST'])
 def add_patient():
     data = request.get_json()
+    if Patient.get_one(data.get('id')):
+        return "patient already exists", 400
     Patient.create(data).__dict__
     
     return "patient successfully added", 200
