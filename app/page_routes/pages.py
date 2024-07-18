ffd38b68d33266339662fe0ae9ae045c8fbae9fc
root <root@runner.>
2024-07-18 14:47:03 +0200

    added the the patient view

--- app/page_routes/pages.py
+++ app/page_routes/pages.py
@@ -16,3 +16,17 @@ def dashboard_doctor():
     """ get all patients"""
     return render_template('doctor/dashboard-doctor.html')
 
+
+@pages.route('/all_patients', strict_slashes=False)
+def all_patients():
+    """ get all patients"""
+    patients = Patient.get_all()
+    print(patients)
+    return render_template('doctor/all-patients.html', patients=patients)
+
+
+@pages.route('/patient_view/<patient_id>', strict_slashes=False)
+def patient_view(patient_id):
+    """ get all patients"""
+    patient = Patient.get_one_by_id(patient_id)
+    return render_template('doctor/patient-view.html', patient=patient)
