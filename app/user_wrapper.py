3154de5c14f3efb2eff208ab48a973d6e253d4e9
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-07-24 01:29:48 +0200

    added func to get firstname, lastname and id

--- app/user_wrapper.py
+++ app/user_wrapper.py
@@ -2,26 +2,35 @@
 
 from flask_login import UserMixin
 
-class UserWrapper(UserMixin):
-    def __init__(self, user_dict, role):
-        self.user_dict = user_dict
+class UserWrapper:
+    def __init__(self, user, role):
+        self.user = user
         self.role = role
-        self.id = user_dict['_id']
-    
-    @property
-    def is_active(self):
-        # Implement logic to check if the user is active
-        return True
-    
+
     @property
     def is_authenticated(self):
-        # Implement logic to check if the user is authenticated
         return True
-    
+
+    @property
+    def is_active(self):
+        return True
+
     @property
     def is_anonymous(self):
-        # Implement logic to check if the user is anonymous
         return False
-    
+
     def get_id(self):
-        return str(self.id)
+        return str(self.user.get('_id'))
+
+    @property
+    def first_name(self):
+        return self.user.get('first_name')
+
+    @property
+    def last_name(self):
+        return self.user.get('last_name')
+
+    @property
+    def _id(self):
+        return self.user.get('_id')
+
